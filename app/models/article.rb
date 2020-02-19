e1838b8fb922bf19a8dc42aea992e2c28416efe3
Evan Feder <evanmfeder@gmail.com>
2020-02-18 21:10:51 -0500

    Article model code

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ApplicationRecord
+  belongs_to :category
+
+  # Validations
+  validates_presence_of :title, :content
+
+  scope :active, 			 -> { where('active = ?', true) }
+  scope :alphabetical, -> { order('title') }
+end
